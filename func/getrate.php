635988ff3f5ed15ea92a59c4916617944d2d29dc
SORA NO RISU <soralis.nem@gmail.com>
2018-02-11 22:14:46 +0900

    ドル円相場取得を修正

--- func/getrate.php
+++ func/getrate.php
@@ -2,17 +2,9 @@
 
 function get_rate(&$USD_JPY)
 {
-	$url = 'http://www.gaitameonline.com/rateaj/getrate';
+	$url = 'http://api.aoikujira.com/kawase/json/USD';
 	$ch = curl_init();
 	curl_setopt_array($ch, [CURLOPT_URL => $url, CURLOPT_RETURNTRANSFER => true, CURLOPT_TIMEOUT => 5, CURLOPT_CONNECTTIMEOUT => 5, ]);
 	$res = json_decode(curl_exec($ch));
-
-
-	foreach ($res->quotes as $quote) {
-		switch ($quote->currencyPairCode) {
-			case 'USDJPY':
-			$USD_JPY = ($quote->ask + $quote->bid) /2;
-			break;
-		}
-	}
+	$USD_JPY = $res->JPY;
 }
